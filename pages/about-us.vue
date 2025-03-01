7d369bc6e4ba978dae6752ce1e7a9c5ee0710d3d
ilhomabdiroziqov <i.abdiroziqov@uic.group>
2025-03-01 23:44:06 +0500

    feat: update about us section with company name and image

--- pages/about-us.vue
+++ pages/about-us.vue
@@ -1,3 +1,10 @@
 <template>
-  <div>hello babe</div>
+  <div>
+    <div>
+            <h3 class="text-3xl leading-130">КОМПАНИЯ "RAZATA"</h3>
+    </div>
+    <div>
+      <img src="/images/about.svg" alt="image">
+    </div>
+  </div>
 </template>
