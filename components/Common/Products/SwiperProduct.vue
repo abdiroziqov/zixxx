3ca434b5fa9810ec2f6470baa1c0c4f60b9bf3d9
ilhomabdiroziqov <i.abdiroziqov@uic.group>
2025-07-04 11:02:14 +0500

    feat: simplify SwiperProduct component by removing unused state management and enabling loop functionality

--- components/Common/Products/SwiperProduct.vue
+++ components/Common/Products/SwiperProduct.vue
@@ -4,61 +4,36 @@ import { Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import { ref } from 'vue';
 
 interface Props {
   images: string[];
 }
 defineProps<Props>();
-
-const swiperInstance = ref<any>(null);
-const isBeginning = ref(true);
-const isEnd = ref(false);
-
-function handleSwiper(swiper: any) {
-  swiperInstance.value = swiper;
-  updateNavState(swiper);
-  swiper.on('slideChange', () => {
-    updateNavState(swiper);
-  });
-}
-
-function updateNavState(swiper: any) {
-  isBeginning.value = swiper.isBeginning;
-  isEnd.value = swiper.isEnd;
-}
 </script>
 
 <template>
   <div class="swiper-container relative w-full max-w-[500px]">
-    <!-- Prev Button -->
     <button
-        class="swiper-button-prev-custom text-2xl absolute group justify-center flex top-1/2 left-2 z-10 -translate-y-1/2 bg-white dark:bg-dark rounded-full p-2 shadow transition-opacity duration-300"
-        :class="{ 'opacity-50 pointer-events-none': isBeginning }"
+        class="swiper-button-prev-custom text-2xl absolute group justify-center flex top-1/2 left-2 z-10 -translate-y-1/2 bg-white dark:bg-dark rounded-full p-2 shadow"
     >
-      <i class="icon-chevron hover:text-orange duration-300 rotate-180" />
+      <i class="icon-chevron hover:text-orange duration-300 rotate-180"/>
+    </button>
+    <button
+        class="swiper-button-next-custom text-2xl absolute justify-center flex top-1/2 right-2 z-10 -translate-y-1/2 bg-white dark:bg-dark rounded-full p-2 shadow"
+    >
+      <i class="icon-chevron hover:text-orange duration-300"/>
     </button>
 
-    <!-- Next Button -->
-    <button
-        class="swiper-button-next-custom text-2xl absolute justify-center flex top-1/2 right-2 z-10 -translate-y-1/2 bg-white dark:bg-dark rounded-full p-2 shadow transition-opacity duration-300"
-        :class="{ 'opacity-50 pointer-events-none': isEnd }"
-    >
-      <i class="icon-chevron hover:text-orange duration-300" />
-    </button>
-
-    <!-- Swiper -->
     <Swiper
         :modules="[Navigation, Pagination]"
         :slides-per-view="1"
         :space-between="10"
-        :loop="false"
+        :loop="true"
         :navigation="{
         nextEl: '.swiper-button-next-custom',
         prevEl: '.swiper-button-prev-custom'
       }"
         :pagination="{ clickable: true }"
-        @swiper="handleSwiper"
         class="rounded-lg overflow-hidden shadow-xl"
     >
       <SwiperSlide v-for="(image, index) in images" :key="index">
